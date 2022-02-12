8c1a88aa5bc91a28fd905ca79847f976a9fd3d6d
Natosha <lalabumlala@gmail.com>
2022-02-12 19:54:59 +0300

    Refactoring

--- TODO/indexArray.js
+++ TODO/indexArray.js
@@ -50,25 +50,22 @@ const options = {
 
 function changeStatus(taskName, newStatus) {
 	list.filter(task => task.name === taskName ? task.status = newStatus : null)
-    return list
 }
 
 function changePriority(taskName, newPriority) {
 	list = list.filter(task => task.name === taskName ? task.priority = newPriority : null)
-	return list
 }
 
 function changeDate(taskName, newDate) {
 	list = list.filter(task => task.name === taskName ? task.date = newDate : null)
-	return list
 }
 
 function addTask(taskName) {
 	list.push ({
         name: taskName,
-        status: options['status'][0],
-        priority: options['priority'][0],
-		date: options['date'][0]
+        status: options.status[0],
+        priority: options.priority[0],
+		date: options.date[0]
     })
 	return list
 }
@@ -79,26 +76,7 @@ function deleteTask(taskName) {
 }
 
 function showList() {
-    let result = ``
-
-	for (let key in options) {
-
-		if (key === 'status') {
-
-			options.status.forEach(currentStatus => {
-				result += `${currentStatus}:\n`
-
-				list.forEach(
-					item => currentStatus === item['status']
-						? result += `${item[`name`]}\n`
-						: null
-				)
-				result += `\n`
-			})
-
-		}
-	}
-    return result
+	showBy('status');
 }
 
 function showBy(condition) {
@@ -112,14 +90,15 @@ function showBy(condition) {
 
 				list.forEach(
 					task => currentCondition === task[condition]
-						? listByCond += `${task[`name`]}\n`
+						? listByCond += ` - ${task[`name`]}\n`
 						: null
 				)
 				listByCond += `\n`
 			})
 		}
 	}
-	return listByCond
+
+	console.log(listByCond)
 }
 
 // console.log(changeStatus('create a post', 'Done'))
@@ -128,4 +107,4 @@ function showBy(condition) {
 // console.log(addTask('have a walk'))
 // console.log(deleteTask('test'))
 // console.log(showList())
-console.log(showBy('date'))
+// console.log(showBy('date'))
