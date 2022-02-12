0e933cd4512b94e4f6c72de56b85c31a8b8c2f48
Natosha <lalabumlala@gmail.com>
2022-02-12 15:38:04 +0300

    feat: add new functions

--- TODO/indexArray.js
+++ TODO/indexArray.js
@@ -1,75 +1,131 @@
 const list = [
-	{name: 'create a post',
-	status: 'In progress',
-	priority: 'low'},
+    {
+        name: 'create a post',
+        status: 'In progress',
+        priority: 'Low',
+		date: 'Tomorrow'
+    },
 
-	{name: 'sleep',
-	status: 'TODO',
-	priority: 'high'},
+    {
+        name: 'sleep',
+        status: 'TODO',
+        priority: 'High',
+		date: 'Some day',
+    },
 
-	{name: 'smile',
-	status: 'TODO',
-	priority: 'Medium'},
+    {
+        name: 'smile',
+        status: 'TODO',
+        priority: 'Medium',
+		date: 'Some day',
+    },
 
-	{name: 'test',
-	status: 'Done',
-	priority: 'high'},
+    {
+        name: 'work',
+        status: 'Done',
+        priority: 'High',
+		date: 'Today',
+    },
+
+    {
+        name: 'test',
+        status: 'Done',
+        priority: 'High',
+		date: 'Today',
+    },
+
+    {
+        name: 'water',
+        status: 'Done',
+        priority: 'Medium',
+		date: 'Every day',
+    },
 ]
-const statuses = {
-	todo: 'TODO',
-	inProgress: 'In progress',
-	done: 'Done'
+
+const options = {
+	priority: ['Low', 'Medium', 'High'],
+	date: ['Today', 'Tomorrow', 'Every day', 'Some day'],
+	status: ['TODO', 'In progress', 'Done'],
 }
-const priority = {
-	high: 'High',
-	medium: 'Medium',
-	low: 'Low'
-}
+
 function changeStatus(taskName, status) {
-	list.forEach(task => task['name'] === taskName ? task['status'] = status : null)
+    list.forEach(task => task['name'] === taskName ? task['status'] = status : null)
+    return list
+}
+function changePriority(taskName, priority) {
+	list.forEach(task => task['name'] === taskName ? task['priority'] = priority : null)
+	return list
+}
+function changeDate(taskName, date) {
+	list.forEach(task => task['name'] === taskName ? task['date'] = date : null)
 	return list
 }
 
 function addTask(taskName) {
-	const newTask = {
-		name: taskName,
-		status: statuses['todo'],
-		priority: priority['low']
-	}
-	list.push(newTask)
-	return newTask
+    const newTask = {
+        name: taskName,
+        status: options['status'][0],
+        priority: options['priority'][0],
+		date: options['date'][0]
+    }
+    list.push(newTask)
+    return newTask
 }
 
 function deleteTask(taskName) {
-	list.map((task, idx) => task['name'] === taskName ? list.splice(idx, 1) : null);
-	return list
+    list.map((task, idx) => task['name'] === taskName ? list.splice(idx, 1) : null);
+    return list
 }
 
 function showList() {
-	let todo = []
-	let inProgress = []
-	let done = []
+    let result = ``
 
-	list.forEach(function (task, index) {
-		if (task.status === statuses.todo) {
-			todo.push(task.name)
-		} else if (task.status === statuses.inProgress) {
-			inProgress.push(task.name)
-		} else {
-			done.push(task.name)
+	for (let key in options) {
+
+		if (key === 'status') {
+
+			for (let i = 0; i < options.status.length; i++) {
+				let currentStatus = options.status[i]
+				result += `${currentStatus}:\n`
+
+				list.forEach(
+					item => currentStatus === item['status']
+						? result += `${item[`name`]}\n`
+						: null
+				)
+				result += `\n`
+			}
 		}
-	})
-
-	let allList = ``
-
-	for(let key in statuses) {
-		allList = `${statuses.todo}:\n${todo}\n\n${statuses.inProgress}:\n${inProgress}\n\n${statuses.done}:\n${done}`
 	}
-
-	return allList
+    return result
 }
 
-// console.log(changeStatus('create a post', 'Done'))
+function showBy(value) {
+	let listOfValue = ``
+
+	for (let key in options) {
+		if (key === value) {
+			for (let i = 0; i < options[value].length; i++) {
+				let currentValue = options[value][i]
+				listOfValue += `${currentValue}:\n`
+
+				list.forEach(
+					item => currentValue === item[value]
+						? listOfValue += `${item[`name`]}\n`
+						: null
+				)
+				listOfValue += `\n`
+			}
+		}
+	}
+	return listOfValue
+}
+
+
+console.log(changeStatus('create a post', 'Done'))
+console.log(changePriority('water', 'High'));
+console.log(changeDate('test', 'tomorrow'));
 // console.log(addTask('have a walk'))
 // console.log(deleteTask('test'))
-console.log(showList())
+// console.log(showList())
+// console.log(showBy('date'))
