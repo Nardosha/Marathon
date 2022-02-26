d5c77f42992eb4d1954fbac665ed8a5330f80692
Natosha <lalabumlala@gmail.com>
2022-02-26 14:07:50 +0300

    wip

--- Calculator/js/index_calc_ui.js
+++ Calculator/js/index_calc_ui.js
@@ -10,30 +10,47 @@ const multBtn = document.querySelector('.mult')
 const subBtn = document.querySelector('.sub')
 const sumBtn = document.querySelector('.sum')
 
+const OPERATIONS = {
+    sum: '+',
+    sub: '-',
+    mult: '*',
+    div: '/',
+}
+
 buttons.addEventListener('click', function (e) {
     let button = e.target
     let actionType = button.dataset.action
-    let action = button.innerHTML
+    let digit = button.innerHTML
+    let number = display.value
+    let result = 0
 
     if (actionType === 'number') {
-        console.log(action)
-        display.value += action
+        // getNumber(digit)
+        number += digit
+        display.value = number
     }
     if (actionType === 'clear') {
         display.value = ''
     }
-    if (actionType === 'clear') {
+    if (actionType === 'delete') {
+        let lastChar = number.length - 1
+        let newValue = number.slice(0, lastChar);
+        display.value = newValue
+    }
+    if (actionType === 'sum' || actionType === 'sub' || actionType === 'mult' || actionType === 'div') {
+        let action = getOperationType(actionType)
+        let number1 = number
+        number = 0
+        display.value = ''
 
+        console.log(number1)
     }
 
-
-    console.log(display)
 })
 
 display.addEventListener('change', function (e) {
     let number = this.value
 })
-
 
 
 function getValue() {
@@ -45,8 +62,18 @@ function calc(a, b) {
     let numberB
 }
 
-const OPERATIONS = {
-    'sum' : '+',
-    'div': '-',
+function getOperationType(type) {
+    if (OPERATIONS[type]) {
+        return OPERATIONS[type]
+    }
 }
+
+function getNumber(number) {
+    let number1 = number
+
+    return number1
+}
+
+getOperationType('sub')
+
 
